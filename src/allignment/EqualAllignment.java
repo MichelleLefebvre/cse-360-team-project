759a66b5fa32188bde21f2a8c14ecde7f4c12fa4
Michelle Lefebvre <mlefebvre1@student.sierracollege.edu>
2019-12-02 17:12:07 -0700

    -e command working

--- src/allignment/EqualAllignment.java
+++ src/allignment/EqualAllignment.java
@@ -4,6 +4,39 @@ public class EqualAllignment extends AllignmentType
 {
 	public String allign(String line, int maxChars)
 	{
-		return line;
+		String[] words = line.split(" ");
+		int numCharacter = 0;
+		String newLine = " ";
+
+		while(numCharacter < maxChars -1) {
+
+			for (int i = 0; i < words.length; i++)
+			{
+				if(numCharacter < maxChars) {
+					words[i] += " ";
+					numCharacter += 1;
+
+				}
+			}
+
+			numCharacter = 0;
+			for (int i = 0; i < words.length; i++)
+			{
+
+				numCharacter += words[i].length();
+				System.out.println(numCharacter);
+			}
+
+		}
+
+		for(int i = 0; i < words.length; i++)
+		{
+			newLine += words[i];
+		}
+
+		System.out.println(newLine);
+
+		return newLine;
 	}
+
 }
